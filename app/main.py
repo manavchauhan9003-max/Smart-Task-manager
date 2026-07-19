c329eb22f73711c7bab9e7deb9a5319d63ee1240
manavchauhan9003 <manavchauhan9003@gmail.com>
2026-07-19 15:49:25 +0530

    learned the get method by adding some tasks

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+task = [
+    {"id": 1, "title": "Learn FastAPI", "Priority": "High", "Completed": "False"},
+    {"id": 2, "title": "Buy Groceries", "Priority": "Low", "Completed": "False"},
+    {"id": 3, "title": "Write report", "Priority": "Medium", "Completed": "True"}
+]
+
 @app.get("/")
 def read_root():
     return{"message": "welcome to the smart task manager"}
@@ -9,4 +15,15 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+@app.get("/tasks")
+def get_tasks():
+    return task
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for t in task:
+        if t["id"] == task_id:
+            return t
+    return {"error": "Task not found"}
 
