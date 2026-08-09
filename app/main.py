ed71fac6c66f9d8450b21ea75f565cee04cd90c4
manavchauhan9003 <manavchauhan9003@gmail.com>
2026-08-09 09:20:09 +0530

    final deployment test

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000","http://127.0.0.1:8000", "http://localhost:8000"],
+    allow_origins=["https://smart-task-manager-psi-three.vercel.app","http://127.0.0.1:3000", "http://localhost:3000","http://127.0.0.1:8000", "http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
